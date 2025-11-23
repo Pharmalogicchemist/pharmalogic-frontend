8b26d71e504616d6c5c569ba5a68f740187dc911
Pharmalogicchemist <pharmalogicweightloosmedicatio@gmail.com>
2025-11-23 16:00:21 +0000

    Update database.js

--- netlify/functions/database.js
+++ netlify/functions/database.js
@@ -2,7 +2,6 @@
 import { neon } from "@neondatabase/client";
 
 const connectionString = process.env.DATABASE_URL;
-
 if (!connectionString) {
   console.error("DATABASE_URL is not set in environment variables");
   throw new Error("DATABASE_URL is missing");
